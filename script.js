92ec105528c691576e80ff86b6f339f010b42c19
Sam Farrell <merriman2948@gmail.com>
2023-02-05 17:13:23 -0500

    Changed if statment that fixed mult by 0 with great than rather than equal to

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ zero.addEventListener('click', () => {
     displayArea.textContent = displayValue.replace(/^0+/, '')
 
     //Makes 0 populate the display when multiplying by 0
-    if(operator === 3) {
+    if(operator > 0) {
         displayArea.textContent = 0;
     } 
 
